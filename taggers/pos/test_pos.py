20ebadeab3779477916c03e73da3c6b572b97b09
Clément Besnier <clemsciences@aol.com>
2019-10-18 00:14:08 +0200

    Added n-grams taggers and corresponding tests

--- taggers/pos/test_pos.py
+++ taggers/pos/test_pos.py
@@ -2,7 +2,6 @@ import unittest
 
 from taggers.pos.reader import read_pickle
 
-
 __author__ = ["Clément Besnier <clemsciences@aol.com>", ]
 
 
@@ -17,6 +16,42 @@ class TestPOSTagger(unittest.TestCase):
         self.assertEqual(tnt_tagger.tag("uns ist in alten mæren wunders vil geseit".split(" ")),
                          [('uns', 'PPER'), ('ist', 'VAFIN'), ('in', 'APPR'), ('alten', 'ADJA'), ('mæren', 'ADJA'),
                           ('wunders', 'NA'), ('vil', 'AVD'), ('geseit', 'VVPP')])
+
+    def test_unigram_tagger(self):
+        unigram_tagger = read_pickle("unigram.pickle")
+        target = [('uns', 'PPER'),
+                  ('ist', 'VAFIN'),
+                  ('in', 'APPR'),
+                  ('alten', 'ADJA'),
+                  ('mæren', 'ADJA'),
+                  ('wunders', 'NA'),
+                  ('vil', 'ADJA'),
+                  ('geseit', 'VVPP')]
+        self.assertEqual(target, unigram_tagger.tag("uns ist in alten mæren wunders vil geseit".split(" ")))
+
+    def test_bigram_tagger(self):
+        bigram_tagger = read_pickle("bigram.pickle")
+        target = [('uns', 'PPER'),
+                  ('ist', 'VAFIN'),
+                  ('in', 'APPR'),
+                  ('alten', 'ADJA'),
+                  ('mæren', 'NA'),
+                  ('wunders', 'NA'),
+                  ('vil', None),
+                  ('geseit', None)]
+        self.assertEqual(target, bigram_tagger.tag("uns ist in alten mæren wunders vil geseit".split(" ")))
+
+    def test_trigram_tagger(self):
+        tri_tagger = read_pickle("trigram.pickle")
+        target = [('uns', 'PPER'),
+                  ('ist', 'VAFIN'),
+                  ('in', 'APPR'),
+                  ('alten', 'ADJA'),
+                  ('mæren', 'NA'),
+                  ('wunders', 'NA'),
+                  ('vil', None),
+                  ('geseit', None)]
+        self.assertEqual(target, tri_tagger.tag("uns ist in alten mæren wunders vil geseit".split(" ")))
 
 
 if __name__ == '__main__':
